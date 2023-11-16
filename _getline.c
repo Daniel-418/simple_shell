70a7f6baa76700af60e93f6cf9e4c1df2b605b24
Daniel <komolafedanielolaoluwa@gmail.com>
2023-11-15 17:34:28 -0800

    added all the files

--- _getline.c
+++ _getline.c
@@ -2,6 +2,24 @@
 
 #define BUFFER_SIZE 1024
 
+
+/**
+ * refill_buffer - refills a buffer
+ * @buffer: the buffer
+ * @buffer_size: the buffer size
+ * @position: the position
+ * @fd: the file descriptor
+ *
+ * Return: The buffer size
+ */
+static ssize_t refill_buffer(char *buffer, ssize_t *buffer_size,
+		ssize_t *position, int fd)
+{
+	*buffer_size = read(fd, buffer, BUFFER_SIZE);
+	*position = 0;
+	return (*buffer_size);
+}
+
 /**
  * _getline - custom getline function implementation
  * @lineptr: pointer to the buffer containing read line
@@ -13,42 +31,31 @@
 ssize_t _getline(char **lineptr, size_t *n, int fd)
 {
 	static char buffer[BUFFER_SIZE];
-	static ssize_t buffer_size = 0;
-	static ssize_t position = 0;
-	size_t len = 0;
-	size_t j;
-	ssize_t i;
+	static ssize_t buffer_size, position;
 	char *new_lineptr;
+	size_t len = 0, j;
+	ssize_t i;
 
 	if (!lineptr || !n || fd < 0)
-		return -1;
-	if (*lineptr == NULL || *n == 0)
-	{
-		*n = BUFFER_SIZE;
-		*lineptr = malloc(*n);
-		if (*lineptr == NULL)
-			return -1;
-	}
+		return (-1);
+	if (*lineptr == NULL)
+		*lineptr = malloc((*n = BUFFER_SIZE));
+	if (*lineptr == NULL)
+		return (-1);
 	while (1)
 	{
-		if (position >= buffer_size)
-		{
-			buffer_size = read(fd, buffer, BUFFER_SIZE);
-			if (buffer_size <= 0)
-				return buffer_size;
-			position = 0;
-		}
+		if (position >= buffer_size && refill_buffer(buffer,
+					&buffer_size, &position, fd) <= 0)
+			return (-1);
 		for (i = position; i < buffer_size; i++)
 		{
 			if (len >= *n - 1)
 			{
 				new_lineptr = malloc(*n + BUFFER_SIZE);
 				if (new_lineptr == NULL)
-					return -1;
+					return (-1);
 				for (j = 0; j < len; j++)
-				{
 					new_lineptr[j] = (*lineptr)[j];
-				}
 				free(*lineptr);
 				*lineptr = new_lineptr;
 				*n += BUFFER_SIZE;
@@ -56,12 +63,11 @@ ssize_t _getline(char **lineptr, size_t *n, int fd)
 			(*lineptr)[len++] = buffer[i];
 			if (buffer[i] == '\n')
 			{
-				position = i + 1;
 				(*lineptr)[len] = '\0';
-				return len;
+				position = i + 1;
+				return (len);
 			}
 		}
 		position = buffer_size;
 	}
 }
-
